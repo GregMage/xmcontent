5522e5ea69a4e4e6eee84811c2101fd6964df34b
GregMage <gregory.mage@gmail.com>
2016-07-12 21:15:25 +0200

    Base pour la partie user
    Fichiers vides

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ You may not change or alter any portion of this comment or credits
+ of supporting developers from this source code or any supporting source code
+ which is considered copyrighted (c) material of the original comment or credit authors.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+/**
+ * xmcontent module
+ *
+ * @copyright       XOOPS Project (http://xoops.org)
+ * @license         GNU GPL 2 (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
+ * @author          Mage Gregory (AKA Mage)
+ */
+include 'header.php';
+$xoopsOption['template_main'] = 'xmcontact_index.tpl';
+include_once XOOPS_ROOT_PATH.'/header.php';
+
+
+include XOOPS_ROOT_PATH.'/footer.php';
