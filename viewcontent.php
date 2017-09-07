f2b7b0a358bc71f797cd74a7f8be0b5ba1cbbd4a
mambax7 <mambax7@gmail.com>
2017-09-07 04:58:17 -0400

    https

--- viewcontent.php
+++ viewcontent.php
@@ -12,7 +12,7 @@
 /**
  * xmcontent module
  *
- * @copyright       XOOPS Project (http://xoops.org)
+ * @copyright       XOOPS Project (https://xoops.org)
  * @license         GNU GPL 2 (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
  * @author          Mage Gregory (AKA Mage)
  */
