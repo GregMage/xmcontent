a6763025d7aacaef95a40e9a6724dcbfd599c324
GregMage <gregory.mage@gmail.com>
2021-08-21 14:26:59 +0200

    Version 1.6 RC1

--- xoops_version.php
+++ xoops_version.php
@@ -34,10 +34,10 @@ $modversion['icons32']        = 'Frameworks/moduleclasses/icons/32';
 $modversion['help']           = 'page=help';
 
 //about
-$modversion['release_date']        = '2021/03/20';
+$modversion['release_date']        = '2021/08/21';
 $modversion['module_website_url']  = 'www.monxoops.fr/';
 $modversion['module_website_name'] = 'MonXoops';
-$modversion['module_status']       = 'Alpha';
+$modversion['module_status']       = 'RC1';
 $modversion['min_php']             = '5.6';
 $modversion['min_xoops']           = '2.5.10';
 $modversion['min_db']              = array('mysql' => '5.0.7', 'mysqli' => '5.0.7');
