96ec19335f2b4fe4a86cd0247eb57fd120ce8b8a
GregMage <gregory.mage@gmail.com>
2022-12-30 15:06:08 +0100

    Changement de version, passage sur le nouveau système de version de xoops. Le module n'est plus compatible avec les versions inférieur à XOOPS 2.5.11-Beta2

--- xoops_version.php
+++ xoops_version.php
@@ -19,7 +19,7 @@
 
 $modversion['dirname']        = basename(__DIR__);
 $modversion['name']           = ucfirst(basename(__DIR__));
-$modversion['version']        = '1.61';
+$modversion['version']        = '1.7.0-Alpha';
 $modversion['description']    = _MI_XMCONTENT_DESC;
 $modversion['credits']        = 'G. Mage';
 $modversion['author']         = 'G. Mage';
@@ -37,9 +37,8 @@ $modversion['help']           = 'page=help';
 $modversion['release_date']        = '2022/01/29';
 $modversion['module_website_url']  = 'www.monxoops.fr/';
 $modversion['module_website_name'] = 'MonXoops';
-$modversion['module_status']       = 'Alpha';
 $modversion['min_php']             = '7.1';
-$modversion['min_xoops']           = '2.5.10';
+$modversion['min_xoops']           = '2.5.11-Beta2';
 $modversion['min_db']              = array('mysql' => '5.0.7', 'mysqli' => '5.0.7');
 
 // Search
